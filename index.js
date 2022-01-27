14bf51d1fdfa2d6fe05ab73d6a341890d82fb79f
Gurbir <devgurbir@gmail.com>
2022-01-27 14:41:28 +0530

    node http server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const sum = require("./sum.js")
+const http = require("http")
 
-console.log(sum(2)(3)(4)())
+// console.log(sum(2)(3)(4)())
+
+const server = http.createServer( (req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({
+        data: "Hello World"
+    }))
+})
+
+server.listen(3001)
